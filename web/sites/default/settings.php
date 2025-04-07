bb164c6a8417a42dde46f7a572e4602eb600430a
Tyler Funk <tcfunk25@gmail.com>
2025-04-07 16:55:03 -0500

    fix composer validate errors

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -6,3 +6,9 @@
 
 include dirname(DRUPAL_ROOT) . '/environments/include.settings.inc';
 
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
